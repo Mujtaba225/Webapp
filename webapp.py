8a118421912d5aae48aac74416645d6c00868d2d
Mujtaba225 <106347272+Mujtaba225@users.noreply.github.com>
2022-09-03 16:15:25 +0500

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 02:11:54 2022
+
+@author: SRC
+"""
+
+import streamlit as st
+import pandas as pd
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+
+
+
+
+
+def main():
+    st.title('Dropdown')
+    data = pd.read_csv(r'C:\Users\SRC\CDA/SMS_data.csv',encoding='unicode_escape')
+    label = st.selectbox("Label",list(data['Label'].unique()))
+    st.table(data[data['Label']==label])
+    #st.table(data)
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__=='__main__':
+    main()
+   
